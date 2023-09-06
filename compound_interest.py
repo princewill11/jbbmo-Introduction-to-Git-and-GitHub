34a4a9233151e53ac5bbfbd2d9d8b4f4e7a3d497
princewill11 <59522847+princewill11@users.noreply.github.com>
2023-09-06 01:12:37 +0100

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -14,9 +14,7 @@
 
 def compound_interest(p, t, r):
     return p * (pow((1 + r / 100), t))
-
-
-if __name__ == "__main__":
+    if __name__ == "__main__":
     p = float(input("Enter the principal amount: "))
     t = float(input("Enter the time period: "))
     r = float(input("Enter the rate of interest: "))
